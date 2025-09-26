b5282ef028d877adade09283702c6469e1ae5e5e
LeandroGuidelli <leandrosoaresguidelli@gmail.com>
2025-09-26 18:47:10 -0300

    Sistema de manutenção

---
+++ src/main/java/br/com/aweb/maintenance_manager_spring/model/Manutencao.java
@@ -0,0 +1,53 @@
+package br.com.aweb.maintenance_manager_spring.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Manutencao {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(nullable = false, length = 100)
+    private String solicitante; 
+
+    @NotBlank
+    @Size(min = 5, max = 255)
+    @Column(nullable = false, length = 255)
+    private String descricaoProblema; 
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    @Column(nullable = false, length = 100)
+    private String item; 
+
+    @NotNull
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataHoraSolicitacao = LocalDateTime.now();
+
+    @DateTimeFormat(iso = ISO.DATE_TIME)
+    @Column(nullable = true)
+    private LocalDateTime dataHoraConclusao;
+}
